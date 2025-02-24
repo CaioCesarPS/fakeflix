b02175ca7c7af115b83ba5da0b5ec8dbc3fc6989
CaioCesarPS <caio_cesar_ps@hotmail.com>
2025-02-23 23:12:31 -0300

    refactor: update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { ContentController } from './http/rest/controller/content.controller';
 import { ContentManagementService } from './core/service/content-management.service';
 import { MediaPlayerService } from './core/service/media-player.service';
 import { ContentRepository } from './persistence/repository/content.repository';
-import { ConfigModule } from './infra/module/config/config.module';
 import { MediaPlayerController } from './http/rest/controller/media-player.controller';
 import { VideoRepository } from './persistence/repository/video.repository';
+import { PersistenceModule } from './persistence/persistence.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [PersistenceModule],
   controllers: [ContentController, MediaPlayerController],
   providers: [
     ContentManagementService,
