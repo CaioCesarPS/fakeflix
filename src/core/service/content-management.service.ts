7af4036d7bded841925dbe1aad2289f406cef103
CaioCesarPS <caio_cesar_ps@hotmail.com>
2025-02-27 08:13:52 -0300

    refactor: update content management service

--- src/core/service/content-management.service.ts
+++ src/core/service/content-management.service.ts
@@ -5,6 +5,7 @@ import { ContentType } from '../enum/content-type.enum';
 import { Movie } from '@src/persistence/entity/movie.entity';
 import { Video } from '@src/persistence/entity/video.entity';
 import { Thumbnail } from '@src/persistence/entity/thumbnail';
+import { ExternalMovieClient } from '@src/http/client/external-movie-rating.client';
 
 export interface CreateMovieData {
   title: string;
@@ -16,14 +17,22 @@ export interface CreateMovieData {
 
 @Injectable()
 export class ContentManagementService {
-  constructor(private readonly contentRepository: ContentRepository) {}
+  constructor(
+    private readonly contentRepository: ContentRepository,
+    private readonly externalMovieRatingClient: ExternalMovieClient,
+  ) {}
 
   public async createMovie(createMovieData: CreateMovieData) {
+    const externalRating = await this.externalMovieRatingClient.getRating(
+      createMovieData.title,
+    );
+
     const contentEntity = new Content({
       title: createMovieData.title,
       description: createMovieData.description,
       type: ContentType.MOVIE,
       movie: new Movie({
+        externalRating,
         video: new Video({
           url: createMovieData.url,
           duration: 10,
